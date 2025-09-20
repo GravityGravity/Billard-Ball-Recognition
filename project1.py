7361b663d6119d9adca7bf9b8753d531c103a570
Gravity <isaakhern@outlook.com>
2025-09-20 00:14:47 -0400

    project1.py:     + Added image cropping to prevent detection     algorithm running beyond billard table borders.             *Adjusted circle coordinate print to             match with this change
        +Added Spot-Ball color detection using color
        averaging using a 13x13 mean kernel
    
        + Added blue circle marking for normal balls
        and red circle markings for spot balls
    
        + Added Spot-Ball identification number to
        terminal output '{X coord, y coord, Spot-ball Identifier}'
    
        + Added total circle printout as first line of terminal output
    
        +
    
        +/- Added and replaced with more concise comments
        + Cleaned up formatting
    
        - Removed 'matplotlib' import
    
        - Removed unused Guassian blur
    
        - Removed unnecessary image displays (too many)

--- project1.py
+++ project1.py
@@ -1,68 +1,73 @@
-# Project 1
+# Billard-Ball-Recognition
 
-# DESC:  This project takes in a variety of images of Billards
+# DESC:  This project takes in a variety of images containing Billards
 #        Tables and spots Billard Balls along the top of the table using openCV
-#        image filtering, edge detection, and shape detection
+#        image filtering, edge detection, and shape detection.
 
-
-# NOTES:
-#   All test images are 1080x1920p
-#   Color of table is always homogenous
-#   RED - White spotball with red dots
-#   Green - other balls
-
-# PHASE 1:
-#   50% Ball Detection rate
-#   50% FAR (Incorrect detections rate)
-#   50% CR (Ball classification rate)
-
-
-import sys  # For command line arguments
+import sys
 import numpy as np
 import cv2 as cv
-import matplotlib as plt
 
 
 def main():
 
-    try:    # Debug: Check image path was passed
+    # Debug: Check image path was passed
+    try:
         print(f'Image path passed: {sys.argv[1]}')
     except IndexError:
         sys.exit('ERROR: No Argument for image path')
 
-    # Read Image (0 = grayscale, 1 = BGR 'Default')
+    # Read image and Check image was read
     orgimg = cv.imread(sys.argv[1], cv.IMREAD_COLOR_BGR)
-    """ Unmodified Image """
 
-    if orgimg is None:  # Debug: Check image was read
+    if orgimg is None:
         sys.exit('ERROR: Could not read image')
 
-    gaussblurimg = cv.GaussianBlur(orgimg, (3, 3), 2)
+    # Cropping image to prevent circles detected outside of pool table
+    height, width = orgimg.shape[:2]
+    x1, y1 = 100, 100
+    x2 = (orgimg.shape[1] - x1)
+    y2 = (orgimg.shape[0] - y1)
 
+    kernel = np.ones((13, 13)) / 169
+
+    orgimg = orgimg[y1:y2, x1:x2]
+
+    # Adding filters + color conversion for image
+    grayimg = cv.cvtColor(orgimg, cv.COLOR_BGR2GRAY)
     bilaterBlurImg = cv.bilateralFilter(orgimg, 30, 150, 150)
-    edges = cv.Canny(bilaterBlurImg, 20, 60)
 
+    # Canny Edge detection with Open Operation
+    edges = cv.Canny(bilaterBlurImg, 20, 60)
     edges = cv.dilate(edges, (3, 3), iterations=5)
     edges = cv.erode(edges, (3, 3), iterations=1)
 
+    # OpenCV Circle Shape Detection Algorithm
     Hcircle = cv.HoughCircles(
-        edges, cv.HOUGH_GRADIENT, dp=2, minDist=25, param1=60, param2=55, minRadius=10, maxRadius=40)
+        edges, cv.HOUGH_GRADIENT, dp=2, minDist=35, param1=60, param2=55, minRadius=10, maxRadius=35)
 
     if Hcircle is not None:
         circleDet = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
+        print(len(Hcircle[0, :]))  # Prints total Circles found
 
-        print(len(Hcircle.squeeze()))
-
+        # Loops: through circles and averages color within circles to print Coordinate + identify white spot-ball
         for (x, y, r) in Hcircle[0, :]:
-            print(f'{int(x)} {int(y)} {int(r)}\n')
-            cv.circle(circleDet, (int(x), int(y)), int(r), (0, 255, 0), 2)
+            spot_ball = 0
+            color = (255, 0, 0)
 
-    # # # Display unmodified image
+            # Color avg for identifying spot ball
+            if (np.sum(grayimg[int(y) - 6: int(y) + 7, int(x) - 6: int(x) + 7] * kernel) >= 220):
+                spot_ball = 1
+                color = (0, 0, 255)
+
+            # Draw Circles onto images + Print Coordinates
+            cv.circle(circleDet, (int(x), int(y)), int(r), color, 2)
+            cv.circle(orgimg, (int(x), int(y)), int(r), color, 2)
+            print(f'{int(x) + x1} {int(y) + y1} {spot_ball}')
+
+    # # # Display unmodified image + Canny Edges
     cv.imshow('unmodified image', orgimg)
-    cv.imshow('gauss filter image', gaussblurimg)
-    cv.imshow('bilateral filter image', bilaterBlurImg)
-    # cv.imshow('Canny edge detector', edges)
-    cv.imshow('Hough Circle', circleDet)
+    cv.imshow('Detected Circle + Canny Edges', circleDet)
     cv.waitKey(0)
 
 
